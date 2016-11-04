8fe970f4d500abc90a6db64caabe3ad6a793a3d5
CNagal <christiannaga1@gmail.com>
2016-11-04 11:40:19 -0700

    First Commit

---
+++ MVCEmail/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCEmail.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace MVCEmail.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("recipient@gmail.com")); 
+                message.From = new MailAddress("sender@outlook.com");  
+                message.Subject = string.Format(model.Name);
+                message.Body = string.Format(body, model.Name, model.Email, model.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "user@outlook.com",  
+                        Password = "password"  
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp-mail.outlook.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ MVCEmail/Models/EmailFormModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCEmail.Models
+{
+    public class EmailFormModel
+    {
+        [Required, Display(Name = "Name")]
+        public string Name { get; set; }
+        [Required, Display(Name = "Email"), EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
